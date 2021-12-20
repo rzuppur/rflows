0ced9d1cc480c0c4db5bca3d1ead70c3abe390ad
Reino Zuppur <reino@reinozuppur.com>
2021-12-20 19:37:26 +0200

    remove collapsed sidebar

--- src/components/Chat/Mainbar/Side/ChatMainbarSide.vue
+++ src/components/Chat/Mainbar/Side/ChatMainbarSide.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 
-  .side.r-elevation-3.r-styled-scrollbar(:class="{ collapsed: showCollapsedSidebar }")
+  .side.r-elevation-3.r-styled-scrollbar(v-if="!hideSidebar")
 
     template(v-if="chatWorkspaces.length")
       .workspace(v-for="workspace in chatWorkspaces")
@@ -17,20 +17,21 @@
 
     .r-buttons
 
-      r-button(v-if="!mqSideCollapse" fullwidth :action="() => { sideCollapsed = !sideCollapsed; }" v-rtip.left="showCollapsedSidebar ? 'Expand side' : null" :icon="showCollapsedSidebar ? 'chevron left' : 'chevron right'")
-        template(v-if="!showCollapsedSidebar") #{""} Collapse side
+      r-button(
+        :disabled="!flowsEmail" fullwidth :action="flowsEmailCopy" icon="mail"
+      ) Copy forward email
 
-      r-button(:disabled="!flowsEmail" fullwidth :action="flowsEmailCopy" v-rtip.left="showCollapsedSidebar ? 'Copy forward email' : null" icon="mail")
-        template(v-if="!showCollapsedSidebar") #{""} Copy forward email
+      r-button(
+        v-if="!autoReadEnabled" fullwidth :disabled="unreadMessages.length === 0 || !chatId" :action="markAllMessagesAsRead" icon="check" icon-color="green"
+      ) Mark chat as read
 
-      r-button(v-if="!autoReadEnabled" fullwidth :disabled="unreadMessages.length === 0 || !chatId" :action="markAllMessagesAsRead" v-rtip.left="showCollapsedSidebar ? 'Mark chat as read' : null" icon="check" icon-color="green")
-        template(v-if="!showCollapsedSidebar") #{""} Mark chat as read
+      r-button(
+        v-if="isMemberOfCurrentChat" fullwidth :disabled="leavingOrJoining || !chatId" :action="leaveChat" icon="user leave"
+      ) Leave chat
 
-      r-button(v-if="isMemberOfCurrentChat" fullwidth :disabled="leavingOrJoining || !chatId" :action="leaveChat" v-rtip.left="showCollapsedSidebar ? 'Leave chat' : null"  icon="user leave")
-        template(v-if="!showCollapsedSidebar") #{""} Leave chat
-
-      r-button(v-else :disabled="leavingOrJoining || !chatId" fullwidth :action="joinChat" v-rtip.left="showCollapsedSidebar ? 'Join chat' : null" icon="user join" icon-color="blue")
-        template(v-if="!showCollapsedSidebar") #{""} Join chat
+      r-button(
+        v-else :disabled="leavingOrJoining || !chatId" fullwidth :action="joinChat" icon="user join" icon-color="blue"
+      ) Join chat
 
     hr
 
@@ -65,32 +66,17 @@
 
 <script>
 
-  import { computed, ref, watch } from "@vue/composition-api";
+  import { computed, ref } from "@vue/composition-api";
   import MessageDisplay from "@/components/Message/MessageDisplay.vue";
 
   // eslint-disable-next-line no-unused-vars
   const main = (props, context) => {
     const currentUserId = computed(() => context.root.$store.currentUser?.id);
 
-    const sideCollapsed = ref(false);
-
-    try {
-      const sideIsCollapsed = localStorage.getItem("sidebarCollapsed");
-      if (sideIsCollapsed && JSON.parse(sideIsCollapsed)) sideCollapsed.value = true;
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.warn(error);
-    }
-
     const leavingOrJoining = ref(false);
 
-    watch(sideCollapsed, (newVal, oldVal) => {
-      if (typeof oldVal === "undefined") return;
-      localStorage.setItem("sidebarCollapsed", JSON.stringify(newVal));
-    });
-
-    const showCollapsedSidebar = computed(() => {
-      return context.root.mqSideCollapse || sideCollapsed.value;
+    const hideSidebar = computed(() => {
+      return context.root.mqSideCollapse;
     });
 
     const isMemberOfCurrentChat = computed(() => {
@@ -148,8 +134,7 @@
     };
 
     return {
-      sideCollapsed,
-      showCollapsedSidebar,
+      hideSidebar,
       leaveChat,
       joinChat,
       leavingOrJoining,
@@ -285,24 +270,6 @@
 
     @media (max-width 1000px)
       padding 10px 10px
-
-    &.collapsed
-      max-width 80px
-      min-width @max-width
-
-      @media (max-width 1000px)
-        max-width 60px
-        min-width @max-width
-
-      .show-wide
-        display none
-
-      .side-button
-        text-align center
-
-      .workspace .logo
-        width 40px
-        height @width
 
     .workspace .logo
       background $color-light-gray-background
